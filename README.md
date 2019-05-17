c39485b0cf3720472730727d19b639b41a01c6d8
Rik Schennink <rikschennink@gmail.com>
2019-05-17 15:42:54 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Image Filter plugin for FilePond
+
+[![npm version](https://badge.fury.io/js/filepond-plugin-image-filter.svg)](https://badge.fury.io/js/filepond)
+
+https://pqina.nl/filepond/docs/patterns/plugins/image-filter/
+
+The Image filter plugin automatically adds a color matrix filter to the file metadata.
+
+The [Image preview plugin](https://github.com/pqina/filepond-plugin-image-preview) uses this information to show the correct preview. The [Image transform plugin](https://github.com/pqina/filepond-plugin-image-transform) uses this information to transform the image before uploading it to the server.
+
+[Demo](https://pqina.github.io/filepond-plugin-image-filter/)
---
+++ src/js/index.js
@@ -0,0 +1,50 @@
+import { isImage } from './utils/isImage';
+
+/**
+ * Image Auto Filter Plugin
+ */
+const plugin = ({ addFilter, utils }) => {
+    const { Type } = utils;
+
+    // subscribe to file load and append required transformations
+    addFilter(
+        'DID_LOAD_ITEM',
+        (item, { query }) =>
+            new Promise((resolve, reject) => {
+                // get file reference
+                const file = item.file;
+
+                // if this is not an image we do not have any business filtering it
+                if (!isImage(file) || !query('GET_ALLOW_IMAGE_FILTER')) {
+                    // continue with the unaltered dataset
+                    return resolve(item);
+                }
+
+                // exit if no color matrix supplies
+                const colorMatrix = query('GET_IMAGE_FILTER_COLOR_MATRIX');
+                if (!colorMatrix || colorMatrix.length !== 20) return resolve(item);
+
+                // the image needs to be filtered
+                item.setMetadata('filter', colorMatrix);
+            })
+    );
+
+    // Expose plugin options
+    return {
+        options: {
+            // Enable or disable image filtering
+            allowImageFilter: [true, Type.BOOLEAN],
+
+            // the filter to apply
+            imageFilterColorMatrix: [null, Type.ARRAY]
+        }
+    };
+};
+
+// fire pluginloaded event if running in browser, this allows registering the plugin when using async script tags
+const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+if (isBrowser) {
+    document.dispatchEvent(new CustomEvent('FilePond:pluginloaded', { detail: plugin }));
+}
+
+export default plugin;
